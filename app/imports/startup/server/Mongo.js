5afdc7f8f8c409d8650c2e692d97dd5aafff25a1
PhatCa <phatca@hawaii.edu>
2024-04-05 23:56:29 -1000

    finish basic setup for user collection list and set up schema for cards

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Cards } from '../../api/card/Card.js';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,22 @@ const addData = (data) => {
   Stuffs.collection.insert(data);
 };
 
+const addCardData = (data) => {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Cards.collection.insert(data);
+};
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
+    Meteor.settings.defaultData.forEach((data) => addData(data));
+  }
+}
+
+if (Cards.collection.find().count() === 0) {
+  if (Meteor.settings.cardData) {
+    console.log('Creating card data');
+    Meteor.settings.cardData.forEach((data) => addCardData(data));
   }
 }
---
+++ app/imports/ui/pages/ListCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Cards } from '../../api/card/Card';
+import CardItem from '../components/CardItem';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+const ListCard = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { ready, cards } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Cards.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Stuff documents
+    const cardItems = Cards.collection.find({}).fetch();
+    return {
+      cards: cardItems,
+      ready: rdy,
+    };
+  }, []);
+  return (ready ? (
+    <div className="d-flex flex-row justify-content-around">
+      <div id="leftside" style={{ flex: 1 }}>
+        <h3>Holder for navbar</h3>
+      </div>
+      <div className="d-flex justify-content-center flex-column text-center align-items-center" id="rightside" style={{ flex: 2 }}>
+        <h1>ProfTCG</h1>
+        <h4>Your Card Collection List</h4>
+        <div className="container text-center">
+          <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3  ">
+            {cards.map((card) => <CardItem key={card._id} card={card} />)}
+
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : <LoadingSpinner />);
+};
+
+export default ListCard;
