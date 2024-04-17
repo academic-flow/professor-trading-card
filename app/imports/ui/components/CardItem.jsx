53c4e7ea3bce4cdce5e1aef4f85f1f3441fcaf4b
adamwins <adamws@hawaii.edu>
2024-04-16 15:22:24 -1000

    added flip functionality to card component

--- app/imports/ui/components/CardItem.jsx
+++ app/imports/ui/components/CardItem.jsx
@@ -1,57 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ReactCardFlip from 'react-card-flip';
 
 const CardItem = ({ card }) => {
+
+  const [flip, setFlip] = useState(false);
   const getTextColor = (rarity) => {
     switch (rarity) {
-    case 'Common':
-      return '#A9A9A9';
-    case 'Uncommon':
-      return '#008000';
-    case 'Mythic':
-      return '#FF4500';
-    default:
-      return 'none';
+      case 'Common':
+        return '#A9A9A9';
+      case 'Uncommon':
+        return '#008000';
+      case 'Mythic':
+        return '#FF4500';
+      default:
+        return 'none';
     }
   };
 
   return (
-    <div className="col" style={{ marginBottom: '20px' }}>
-      <Card style={{ width: '18rem' }}>
-        <Card.Title>{card.name}</Card.Title>
-        <div style={{ maxHeight: '200px', overflow: 'hidden' }}>
-          <Card.Img
-            variant="top"
-            src={card.image}
-            height={200}
-            style={{ objectFit: 'contain' }}
-          />
+      <ReactCardFlip isFlipped={flip} flipDirection={"right"}>
+        <div className="col" style={{ marginBottom: '20px' }}>
+          <Card onClick={() => setFlip(!flip)} style={{ width: '18rem' }}>
+            <Card.Title>{card.name}</Card.Title>
+            <div style={{ maxHeight: '200px', overflow: 'hidden' }}>
+              <Card.Img
+                  variant="top"
+                  src={card.image}
+                  height={200}
+                  style={{ objectFit: 'contain' }}
+              />
+            </div>
+            <Card.Body style={{ maxHeight: 'none', overflow: 'hidden', height: '100px' }}>
+              <Card.Text>
+                Rarity: <span style={{ color: getTextColor(card.rarity) }}>{card.rarity}</span>
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </div>
-        <Card.Body style={{ maxHeight: 'none', overflow: 'hidden', height: '100px' }}>
-          <Card.Text>
-            Rarity: <span style={{ color: getTextColor(card.rarity) }}>{card.rarity}</span>
-          </Card.Text>
-        </Card.Body>
-        <Link to={`/edit/${card._id}`}>
-          <Button variant="primary">Details</Button>
-        </Link>
-      </Card>
-    </div>
+        <div className="col" style={{ marginBottom: '20px' }}>
+          <Card onClick={() => setFlip(!flip)} style={{ width: '18rem' }}>
+            <Card.Title>{card.name}</Card.Title>
+            <Card.Body style={{ maxHeight: 'none', overflow: 'hidden', height: '300px' }}>
+              <Card.Text>
+                Professor Details:
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </ReactCardFlip>
   );
 };
 
-CardItem.propTypes = {
-  card: PropTypes.shape({
-    name: PropTypes.string,
-    image: PropTypes.string,
-    description: PropTypes.string,
-    rarity: PropTypes.string,
-    owner: PropTypes.string,
-    _id: PropTypes.string,
-  }).isRequired,
-};
 
 export default CardItem;
