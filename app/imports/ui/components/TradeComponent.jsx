ec1bbaae18c3352b3701d2a09c620ddd69d89932
PhatCa <phatca@hawaii.edu>
2024-04-18 16:22:29 -1000

    create tradecomponent file

---
+++ app/imports/ui/components/TradeComponent.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ReactCardFlip from 'react-card-flip';
+
+const TradeItem = ({ request }) => (
+  
+);
