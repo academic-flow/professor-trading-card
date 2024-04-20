4a8c03a62179593aaae2c23408b0d3bdedb60a9b
seanf2UH <156386596+seanf2UH@users.noreply.github.com>
2024-04-20 09:50:37 -1000

    navbar and csschanges

--- app/imports/ui/components/NavBarAdmin.jsx
+++ app/imports/ui/components/NavBarAdmin.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import App from '../layouts/App';
 import '../../../public/style/navbar.css';
-import { Collection, House, CardList, People, List, PeopleFill } from 'react-bootstrap-icons';
+import { House, CardList, List } from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Roles } from 'meteor/alanning:roles';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../layouts/App.jsx';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Landing from '../pages/Landing';
+import NotFound from '../pages/NotFound';
+import SignOut from '../pages/SignOut';
+import CardItem from './CardItem';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import NotAuthorized from '../pages/NotAuthorized';
+import LoadingSpinner from './LoadingSpinner';
 
 const NavBar = () => (
   <div className="d-flex flex-column min-vh-100">
@@ -12,23 +24,13 @@ const NavBar = () => (
         <div className="col-auto px-0">
           <div id="sidebar" className="collapse collapse-horizontal col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-danger">
             <div id="sidebar-nav" className="d-flex flex-column align-items-center align-items-sm-start px-1 pt-2 text-white min-vh-100">
-              <a href="/signin" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+              <a href="/admin" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <span className="collapse fs-5 d-none d-sm-inline">Menu</span>
               </a>
               <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                 <li className="nav-item">
-                  <a href="/mainpage" className="nav-link px-0 align-middle">
+                  <a href="/home" className="nav-link px-0 align-middle">
                     <House className="fs-4 text-white" /><span className="ms-1 d-none d-sm-inline text-white">Home</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="/list" className="nav-link px-0 align-middle">
-                    <Collection className="fs-4 text-white" /> <span className="ms-1 d-none d-sm-inline text-white">Collection</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="/friendlist" className="nav-link px-0 align-middle">
-                    <People className="fs-4 text-white" /><span className="ms-1 d-none d-sm-inline text-white">Friends</span>
                   </a>
                 </li>
                 <li className="nav-item">
@@ -41,7 +43,7 @@ const NavBar = () => (
               <div className="dropdown pb-4">
                 <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                   <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="morning-brew-coffee.png" width="30" height="30" className="rounded-circle" />
-                  <span className="d-none d-sm-inline mx-1">*username*</span>
+                  <span className="d-none d-sm-inline mx-1">*admin*</span>
                 </a>
                 <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
                   <li><a className="dropdown-item" href="/signout">Sign out</a></li>
@@ -51,12 +53,11 @@ const NavBar = () => (
           </div>
         </div>
         <main className="col ps-md-2 pt-2">
-          <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" className="border rounded-3 p-2 text-decoration-none "><List className="fs-4" /></a>
+          <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" className="border rounded-3 p-2 text-decoration-none "><List className="fs-4" /> Menu</a>
         </main>
       </div>
     </div>
   </div>
 );
-
 
 export default NavBar;
--- app/imports/ui/pages/MainPage.jsx
+++ app/imports/ui/pages/MainPage.jsx
@@ -5,84 +5,81 @@ import styles from '../../../public/stylesheets/MainPage.css';
 
 // Reusable Section component
 const Section = ({ title, children }) => (
-    <section className="text-center" style={{ marginBottom: '40px' }}>
-      <h2 style={{ color: '#333', fontSize: '28px', fontWeight: 'bold', margin: '20px 0' }}>{title}</h2>
-      <Container>
-        <Row md={1} lg={2}>
-          {children}
-        </Row>
-      </Container>
-    </section>
+  <section className="text-center" style={{ marginBottom: '40px' }}>
+    <h2 style={{ color: '#333', fontSize: '28px', fontWeight: 'bold', margin: '20px 0' }}>{title}</h2>
+    <Container>
+      <Row md={1} lg={2}>
+        {children}
+      </Row>
+    </Container>
+  </section>
 );
 
-function MainPage() {
-  return (
-      <main>
+const MainPage = () => (
+  <main>
 
-        <Container fluid>
-          <Row>
-            <Col></Col>
-            <Col sm={6} className="text-center">
-              <h1 style={{ fontSize: '48px', fontWeight: 'bold', color: '#333', marginTop: '40px' }}>Welcome to ProfTCG</h1>
-            </Col>
-            <Col className="d-flex align-items-center justify-content-end">
-              <PersonCircle size={64} style={{ marginBottom: '30px' }}/>
-            </Col>
-          </Row>
-          <hr className="line"/>
-        </Container>
+    <Container fluid>
+      <Row>
+        <Col />
+        <Col sm={6} className="text-center">
+          <h1 style={{ fontSize: '48px', fontWeight: 'bold', color: '#333', marginTop: '40px' }}>Welcome to ProfTCG</h1>
+        </Col>
+        <Col className="d-flex align-items-center justify-content-end">
+          <PersonCircle size={64} style={{ marginBottom: '30px' }} />
+        </Col>
+      </Row>
+      <hr className="line" />
+    </Container>
 
-        <Container className="bg-image" fluid>
-          <Container className="justify-content-center text-center">
-            <h2 style={{ color: '#333', fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>Available Packages To Open:</h2>
-            <p style={{ color: '#666', fontSize: '18px', marginBottom: '20px' }}>Click here to open your package:</p>
-            <Button variant="primary" size="lg" style={{ backgroundColor: '#333', borderColor: '#333' }}>
-              <GiftFill size={64}/>
-            </Button>
-          </Container>
+    <Container className="bg-image" fluid>
+      <Container className="justify-content-center text-center">
+        <h2 style={{ color: '#333', fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>Available Packages To Open:</h2>
+        <p style={{ color: '#666', fontSize: '18px', marginBottom: '20px' }}>Click here to open your package:</p>
+        <Button variant="primary" size="lg" style={{ backgroundColor: '#333', borderColor: '#333' }}>
+          <GiftFill size={64} />
+        </Button>
+      </Container>
 
-          <br />
-          <br />
+      <br />
+      <br />
 
-          {/* Reusable Section component for Trade Requested */}
-          <Section title="Trade requested from 'UserName'">
-            <Col xs={6}></Col>
-            <Col xs={6}></Col>
-          </Section>
+      {/* Reusable Section component for Trade Requested */}
+      <Section title="Trade requested from 'UserName'">
+        <Col xs={6} />
+        <Col xs={6} />
+      </Section>
 
-          <br />
-          <br />
+      <br />
+      <br />
 
-          <Container>
-            <Row>
-              <Col xs={6}>
-                <Section title="Card Offered:">
-                  <Col xs={6}></Col>
-                  <Col xs={6}></Col>
-                </Section>
-              </Col>
-              <Col xs={6}>
-                <Section title="Card Wanted:">
-                  <Col xs={6}></Col>
-                  <Col xs={6}></Col>
-                </Section>
-              </Col>
-            </Row>
-          </Container>
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <Section title="Card Offered:">
+              <Col xs={6} />
+              <Col xs={6} />
+            </Section>
+          </Col>
+          <Col xs={6}>
+            <Section title="Card Wanted:">
+              <Col xs={6} />
+              <Col xs={6} />
+            </Section>
+          </Col>
+        </Row>
+      </Container>
 
-          <br />
-          <br />
-          <br />
+      <br />
+      <br />
+      <br />
 
-          <div className="text-center">
-            <p style={{ color: '#666', fontSize: '18px', marginBottom: '20px' }}>Accept Trade?</p>
-            <Button style={{ marginRight: '10px', backgroundColor: '#00ab66', borderColor: '#00ab66' }}>Yes</Button>
-            <Button style={{ backgroundColor: '#e13d30', borderColor: '#e13d30' }}>No</Button>
-          </div>
-        </Container>
-      </main>
-  );
-}
+      <div className="text-center">
+        <p style={{ color: '#666', fontSize: '18px', marginBottom: '20px' }}>Accept Trade?</p>
+        <Button style={{ marginRight: '10px', backgroundColor: '#00ab66', borderColor: '#00ab66' }}>Yes</Button>
+        <Button style={{ backgroundColor: '#e13d30', borderColor: '#e13d30' }}>No</Button>
+      </div>
+    </Container>
+  </main>
+);
 
 export default MainPage;
-
