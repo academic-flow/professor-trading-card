147933aaf4e2463de358b1972605ef603e983ba1
Jakin Wade <jakinw@hawaii.edu>
2024-04-25 16:14:10 -1000

    Just messing with some stuffs

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Logo from '../components/landingLogo';
+// import '/public/style/landingPageBackground.css';
 
 const Landing = () => (
-    <Container id="landing-page" fluid className="py-3">
+  <div className="bg-image"
+       style={{
+        backgroundImage: url""
+  }}>
+    <Container
+      id="landing-page"
+      fluid
+      className="py-3"
+    >
 
       <Row className="justify-content-center">
-        <Col xs={6} className="text-center">
+        <Col
+          xs={6}
+          className="text-center"
+        >
           <Logo />
         </Col>
       </Row>
 
-
       <Row className="justify-content-center mt-5">
-        <Col xs={12} className="text-center">
+        <Col
+          xs={12}
+          className="text-center"
+        >
           <h1 className="display-4">Welcome to ProfTCG!</h1>
         </Col>
       </Row>
-<br />
+      <br />
       <br />
       <Row className="justify-content-center mt-5">
-        <Col xs={12} md={8} lg={6} className="text-center">
+        <Col
+          xs={12}
+          md={8}
+          lg={6}
+          className="text-center"
+        >
           <p className="lead">
             Open packs of cards containing professors here at UH Manoa, trade with friends, and get to know your professors better!
           </p>
         </Col>
       </Row>
     </Container>
+  </div>
 );
 
 export default Landing;
