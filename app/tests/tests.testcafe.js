c519c127711c45a5d4c5738e815a45236306858f
seanf2UH <156386596+seanf2UH@users.noreply.github.com>
2024-04-27 22:04:05 -1000

    v1navbartestcases

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -9,7 +9,7 @@ import { friendPage } from './friend.page';
 
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'john@foo.com', password: 'changeme' };
-
+const credentialsAdmin = { username: 'admin@foo.com', password: 'changeme' };
 fixture('meteor-application-template-react localhost test with default db')
   .page('http://localhost:3000');
 
@@ -17,19 +17,47 @@ test('Test that landing page shows up', async (testController) => {
   await landingPage.isDisplayed(testController);
 });
 
-test('Test that signin and signout work', async (testController) => {
+test('Test that signin, open menu, and signout work', async (testController) => {
   await navBar.gotoSignInPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
+  await testController.click('#openMenu');
+  await testController.wait(3000);
+  await testController.click('#dropdownUser1');
+  await testController.click('#accountClickSignOut');
 });
 
-test('Test friend list page show up and collection page', async (testController) => {
+test('Test Friendlist works in regular User', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await testController.click('#openMenu');
   await navBar.gotoFriendListPage(testController);
-  await testController.click('#view-collection-button');
-  await testController.click('#trade');
-  await testController.click('.btn-close');
-  await navBar.gotoCardListPage(testController);
-  await testController.click('.card');
-  await testController.wait(5000); 
+});
 
+test('Test Collection works in regular User', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await testController.click('#openMenu');
+  await navBar.gotoCollectionListPage(testController);
+});
+
+test('Test Home works in regular User', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await testController.click('#openMenu');
+  await navBar.gotoHomeListPage(testController);
+});
+
+test('Test Adminlogin and home button works', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentialsAdmin.username, credentialsAdmin.password);
+  await testController.click('#openMenuAdmin');
+  await navBar.gotoHomeAdminListPage(testController);
+});
+
+test('Test CardAdmin button works', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentialsAdmin.username, credentialsAdmin.password);
+  await testController.click('#openMenuAdmin');
+  await navBar.gotoCardAdminListPage(testController);
 });
 
