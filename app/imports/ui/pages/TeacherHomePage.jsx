564951fbd3247d16474f68b365f8a4f84034efda
seanf2UH <156386596+seanf2UH@users.noreply.github.com>
2024-04-30 16:08:07 -1000

    reverted teacher page

--- app/imports/ui/pages/TeacherHomePage.jsx
+++ app/imports/ui/pages/TeacherHomePage.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { Col, Container, Row, Image } from 'react-bootstrap';
-import { Meteor } from 'meteor/meteor';
-import { useTracker } from 'meteor/react-meteor-data';
 
-const TeacherHome = () => {
-const { currentUser } = useTracker(() => ({
-  currentUser: Meteor.user() ? Meteor.user().username : ''
-}), []);
-return (
+const TeacherHome = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
       <Col>
-        <h1>{ currentUser }</h1>
-        <Image src="/images/Admin_Icon.jpeg" width="300" height="300" roundedCircle />
+        <h1>Username</h1>
+        <Image src="/images/circle.png" roundedCircle />
       </Col>
       <Col xs={8} className="d-flex flex-column justify-content-center">
         <h1>Teacher Home Page</h1>
@@ -21,5 +15,5 @@ return (
     </Row>
   </Container>
 );
-}
+
 export default TeacherHome;
